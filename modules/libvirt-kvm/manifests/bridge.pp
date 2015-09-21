9141266e44f0167437d7951dda122407e46dc6ef
narigacdo <narigacdo@gmail.com>
2015-09-21 15:56:49 -0300

    bridge para debian

--- modules/libvirt-kvm/manifests/bridge.pp
+++ modules/libvirt-kvm/manifests/bridge.pp
@@ -1,12 +1,19 @@
 class libvirt-kvm::bridge{
 	$bridge = $bridge_port
+	$ip = $bridge_ip
+	$mask = $bridge_mask
+	$gw = $default_gw
+	$dns = $nameservers
+
+	$path = '/etc/network/interfaces'
 	require libvirt-kvm::server
 
 	file { 'iface-conf':
+		ensure => present,
 		content => template("libvirt-kvm/iface.$operatingsystem.erb"),
 		path => $path,
 		mode => 0644,
 		group => 'root',
-		user => 'root',
+		owner => 'root',
 	}
 }
