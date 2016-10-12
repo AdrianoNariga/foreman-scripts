77634f9bb4a2e3b0c3af3896bd68457cb6cbc4d2
usuario de gerencia <nariga@vostro.home.jab>
2016-10-12 17:30:03 -0300

    separando por so module basics

--- modules/basics-confs/manifests/init.pp
+++ modules/basics-confs/manifests/init.pp
@@ -1,5 +1,14 @@
 class basics-confs {
-	$packages = ['screen','git','vim-enhanced']
+        if $operatingsystem == 'Debian' {
+		$packages = ['screen','git','vim']
+        }
+        elsif $operatingsystem == 'Ubuntu' {
+		$packages = ['screen','git','vim']
+        }
+        elsif $operatingsystem == 'CentOS' {
+		$packages = ['screen','git','vim-enhanced','telnet']
+        }
+
 	package { $packages:
 		ensure => present,
 	}
