31cc92baf32b71c2654c4f1636abdbeefe5d77c2
root <root@dhcp.local>
2017-05-29 19:52:04 -0300

    colocando iptables masquerade

--- foreman/03-dhcp-proxy.sh
+++ foreman/03-dhcp-proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export foreman_hostname=foreman.local
 export ip_foreman=192.168.111.100
-export proxy_hostname=proxy.local
+export proxy_hostname=dhcp.local
 export ip_proxy="$(ip -o -4 a s $(ip r s | grep default | awk '{print $5}' | head -n1) | awk '{print $4}' | cut -d \/ -f 1)"
 source gen_proxy.sh
 
@@ -24,10 +24,16 @@ foreman-installer \
   --foreman-proxy-dhcp-interface=$dhcp_iface \
   --foreman-proxy-dhcp-gateway=$gateway \
   --foreman-proxy-dhcp-range="$range" \
-  --foreman-proxy-dhcp-nameservers="$dns"
+  --foreman-proxy-dhcp-nameservers="$dns" \
+  --foreman-proxy-trusted-hosts=$foreman_hostname \
+  --foreman-proxy-trusted-hosts=$proxy_hostname \
+  --foreman-proxy-foreman-base-url=https://$foreman_hostname \
+  --foreman-proxy-oauth-consumer-key="$consumer_key" \
+  --foreman-proxy-oauth-consumer-secret="$consumer_secret"
 
-net=$(ip -o -4 a s $dhcp_iface | awk '{print $4}')
-ssh -t $ip_foreman "route add -net $net gw $ip_proxy"
+#net=$(ip -o -4 a s $dhcp_iface | awk '{print $4}')
+#ssh -t $ip_foreman "ip route add $net dev \$\(ip r s | grep default | awk \'\{print \$5\}\'\)"
 
-iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -nL POSTROUTING | grep "MASQUERADE  all  --  0.0.0.0/0            0.0.0.0/0" ||
+  iptables -t nat -A POSTROUTING -j MASQUERADE
 sysctl -w net.ipv4.ip_forward=1
