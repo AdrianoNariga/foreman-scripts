46f53bc350b9b1dcb5d0d8070bbbc423acb34978
NarigaCDO <narigacdo@gmail.com>
2017-05-29 00:21:06 -0300

    m

--- foreman/03-dhcp-proxy.sh
+++ foreman/03-dhcp-proxy.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+export foreman_hostname=foreman.local
+export ip_foreman=192.168.111.100
+export proxy_hostname=proxy.local
+export ip_proxy="$(ip -o -4 a s $(ip r s | grep default | awk '{print $5}' | head -n1) | awk '{print $4}' | cut -d \/ -f 1)"
+source gen_proxy.sh
+
 range="192.168.11.2 192.168.11.13"
 dns="192.168.11.14,8.8.8.8"
 gateway=192.168.11.1
 dhcp_iface=eth1
-
-export foreman_hostname=foreman.local
-export ip_foreman=192.168.111.100
-
-export proxy_hostname=proxy.local
-export ip_proxy="$(ip -o -4 a s $(ip r s | grep default | awk '{print $5}' | head -n1) | awk '{print $4}' | cut -d \/ -f 1)"
-
-source gen_proxy.sh
 
 foreman-installer \
   --no-enable-foreman \
