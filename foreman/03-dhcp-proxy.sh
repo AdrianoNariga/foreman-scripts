660bcb6f9a3a0f7219842bb52213caadd0ee606a
NarigaCDO <narigacdo@gmail.com>
2017-05-28 21:37:32 -0300

    arrumando dhcp script

--- foreman/03-dhcp-proxy.sh
+++ foreman/03-dhcp-proxy.sh
@@ -4,22 +4,26 @@ dns="192.168.11.14,8.8.8.8"
 gateway=192.168.11.1
 dhcp_iface=eth1
 
-dhcp_proxy(){
-	foreman-installer \
-	  --no-enable-foreman \
-	  --no-enable-foreman-cli \
-	  --no-enable-foreman-plugin-bootdisk \
-	  --no-enable-foreman-plugin-setup \
-	  --no-enable-puppet \
-	  --enable-foreman-proxy \
-	  --foreman-proxy-puppet=false \
-	  --foreman-proxy-puppetca=false \
-	  --foreman-proxy-tftp=false \
-	  --foreman-proxy-dhcp=true \
-	  --foreman-proxy-dhcp-interface=$dhcp_iface \
-	  --foreman-proxy-dhcp-gateway=$gateway \
-	  --foreman-proxy-dhcp-range="$range" \
-	  --foreman-proxy-dhcp-nameservers="$dns"
-}
+export foreman_hostname=foreman.local
+export ip_foreman=192.168.111.100
 
-dhcp_proxy
+export proxy_hostname=proxy.local
+export ip_proxy="$(ip -o -4 a s $(ip r s | grep default | awk '{print $5}' | head -n1) | awk '{print $4}' | cut -d \/ -f 1)"
+
+source gen_proxy.sh
+
+foreman-installer \
+  --no-enable-foreman \
+  --no-enable-foreman-cli \
+  --no-enable-foreman-plugin-bootdisk \
+  --no-enable-foreman-plugin-setup \
+  --no-enable-puppet \
+  --enable-foreman-proxy \
+  --foreman-proxy-puppet=false \
+  --foreman-proxy-puppetca=false \
+  --foreman-proxy-tftp=false \
+  --foreman-proxy-dhcp=true \
+  --foreman-proxy-dhcp-interface=$dhcp_iface \
+  --foreman-proxy-dhcp-gateway=$gateway \
+  --foreman-proxy-dhcp-range="$range" \
+  --foreman-proxy-dhcp-nameservers="$dns"
