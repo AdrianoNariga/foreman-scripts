8c249318c28f632d0d9c8b7de66fcec68103a9b4
usuario master <root@virsh-host01.home.jab>
2017-05-29 20:14:08 +0000

    funcao para preparar instancia no libvirt

--- foreman/00-virsh-create.sh
+++ foreman/00-virsh-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name=foreman.local
-smart_proxys="proxy.local puppet.local"
+smart_proxys="dhcp.local dns.local puppet.local"
 
 ls /var/lib/libvirt/images/disks/$name || \
   cp /var/lib/libvirt/images/templates/centos /var/lib/libvirt/images/disks/$name
@@ -15,7 +15,7 @@ virsh list --all | grep $name || \
 
 for name in $smart_proxys
 do
-	ram=1024
+	ram=512
 	ls /var/lib/libvirt/images/disks/$name || \
 	  cp /var/lib/libvirt/images/templates/centos /var/lib/libvirt/images/disks/$name
 	sync
@@ -26,3 +26,11 @@ do
 	    --disk path=/var/lib/libvirt/images/disks/$name,device=disk,bus=virtio \
 	    --graphics type=spice --os-type linux --os-variant rhl7.3
 done
+
+prepare_instances(){
+	vms="192.168.111.137 192.168.111.138 192.168.111.139 192.168.111.140"
+	for i in $vms
+	do
+		ssh -t root@$i 'yum install vim git -y ; git clone https://github.com/narigacdo/home-jab.git ; bash /root/home-jab/foreman/set_fixed_ip.sh'
+	done
+}
