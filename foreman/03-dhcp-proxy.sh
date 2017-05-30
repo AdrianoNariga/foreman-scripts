761e51db4b3ba77fb6ab239ef2e9170af2707bcc
Your Name <you@example.com>
2017-05-30 11:59:55 -0300

    diminuindo range dhcp

--- foreman/03-dhcp-proxy.sh
+++ foreman/03-dhcp-proxy.sh
@@ -5,7 +5,7 @@ export proxy_hostname=dhcp.local
 export ip_proxy="$(ip -o -4 a s $(ip r s | grep default | awk '{print $5}' | head -n1) | awk '{print $4}' | cut -d \/ -f 1)"
 source gen_proxy.sh
 
-range="192.168.11.2 192.168.11.13"
+range="192.168.11.2 192.168.11.12"
 dns="192.168.11.14,8.8.8.8"
 gateway=192.168.11.1
 dhcp_iface=eth1
