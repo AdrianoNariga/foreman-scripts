52df844b85004c9cf3011ea0dd33d046fa4537f1
NarigaCD0 <narigacdo@gmail.com>
2017-06-02 02:47:43 +0000

    arrumando

--- foreman/05-tftp_server.sh
+++ foreman/05-tftp_server.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-tftp_server=192.168.11.14
+export foreman_hostname=foreman.local
+export ip_foreman=192.168.111.100
+
+export proxy_hostname=tftp.local
+export ip_proxy="$(ip -o -4 a s $(ip r s | grep default | awk '{print $5}' | head -n1) | awk '{print $4}' | cut -d \/ -f 1)"
+
+source gen_proxy.sh
 
 foreman-installer \
+  --no-enable-foreman \
+  --no-enable-foreman-cli \
+  --no-enable-foreman-plugin-bootdisk \
+  --no-enable-foreman-plugin-setup \
+  --no-enable-puppet \
+  --enable-foreman-proxy \
+  --puppet-server=false \
+  --foreman-proxy-puppet=false \
+  --foreman-proxy-puppetca=false \
   --foreman-proxy-tftp=true \
-  --foreman-proxy-tftp-servername=$tftp_server
+  --foreman-proxy-tftp-servername=$ip_proxy \
+  --foreman-proxy-trusted-hosts=$foreman_hostname \
+  --foreman-proxy-trusted-hosts=$proxy_hostname \
+  --foreman-proxy-foreman-base-url=https://$foreman_hostname \
+  --foreman-proxy-oauth-consumer-key="$consumer_key" \
+  --foreman-proxy-oauth-consumer-secret="$consumer_secret"
+
+#tftp_server=192.168.11.14
+#foreman-installer \
+#  --foreman-proxy-tftp=true \
+#  --foreman-proxy-tftp-servername=$tftp_server
--- libvirtd/00-virsh-create.sh
+++ libvirtd/00-virsh-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name=foreman.local
-smart_proxys="dhcp.local dns.local puppet.local"
+smart_proxys="dhcp.local dns.local tftp.local puppet.local"
 
 ls /var/lib/libvirt/images/disks/$name || \
   cp /var/lib/libvirt/images/templates/centos /var/lib/libvirt/images/disks/$name
@@ -31,6 +31,7 @@ prepare_instances(){
 	vms="192.168.111.137 192.168.111.138 192.168.111.139 192.168.111.140"
 	for i in $vms
 	do
-		ssh -t root@$i 'yum install vim git -y ; git clone https://github.com/narigacdo/home-jab.git ; bash /root/home-jab/foreman/set_fixed_ip.sh'
+		ssh -t root@$i '
+		yum install vim git -y ; git clone https://github.com/narigacdo/home-jab.git ; bash /root/home-jab/foreman/set_fixed_ip.sh'
 	done
 }
