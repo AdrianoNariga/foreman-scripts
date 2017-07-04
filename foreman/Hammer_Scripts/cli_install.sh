6381fa535e2f873499b65d03731757935dbf7f9a
Nariga <alrodrigues@uoldiveo.com>
2017-07-04 14:48:57 -0300

    arrumando script de cli foreman

--- foreman/Hammer_Scripts/cli_install.sh
+++ foreman/Hammer_Scripts/cli_install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+foreman_server=$1
+
 echo "deb http://deb.theforeman.org/ xenial 1.15" > /etc/apt/sources.list.d/foreman.list
-sudo apt-get -y install ca-certificates
-sudo wget -q https://deb.theforeman.org/pubkey.gpg -O- | sudo apt-key add -
-sudo apt-get update && sudo apt-get install ruby-hammer-cli ruby-hammer-cli-foreman
+apt-get -y install ca-certificates
+wget -q https://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+apt-get update && sudo apt-get install ruby-hammer-cli ruby-hammer-cli-foreman -y
 mkdir ~/.hammer/cli.modules.d/ -p
 cat > ~/.hammer/cli.modules.d/foreman.yml <<EOF
 :foreman:
-    :host: 'https://foreman.local/'
+    :host: 'https://$foreman_server/'
     :username: 'nariga'
     :password: '$senha'
     :refresh_cache: false
     :request_timeout: 120
 EOF
 
-sudo hammer --fetch-ca-cert https://foreman.local/
-sudo install /home/nariga/.hammer/certs/foreman.local_443.pem /usr/local/share/ca-certificates/
+sudo hammer --fetch-ca-cert https://$foreman_server/
+sudo install ~/.hammer/certs/${foreman_server}_443.pem /usr/local/share/ca-certificates/
 sudo update-ca-certificates
