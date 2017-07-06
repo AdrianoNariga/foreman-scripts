b24ddd22e822f578659eed7f3316585bd0f110a1
NarigaCDO <narigacdo@gmail.com>
2017-07-06 23:55:06 +0000

    dasdaw

--- foreman/server_foreman.sh
+++ foreman/server_foreman.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-source HOSTS
+source $1
 ip_foreman="$(ip -o -4 a s $(ip r s | grep default | awk '{print $5}' | head -n1) | awk '{print $4}' | cut -d \/ -f 1)"
-
 hostnamectl set-hostname $foreman_hostname
-
 grep $foreman_hostname /etc/hosts || echo "$ip_foreman $foreman_hostname" >> /etc/hosts
-
 source shell-functions/installer-repo.sh
 
 foreman-installer \
@@ -25,16 +22,16 @@ hammer os create --name Debian --architectures x86_64 \
 hammer os create --name CoreOS --architectures x86_64 \
 	    --family CoreOS --major 1437 --minor 2.0 --description "CoreOS Beta 1437"
 
-hammer template list | grep centos_finish ||
+hammer template list | grep -q centos_finish ||
   hammer template create --file Template_Scripts/centos_finish --type finish --name centos_finish
 
-hammer template list | grep debian9_finish ||
+hammer template list | grep -q debian9_finish ||
   hammer template create --file Template_Scripts/debian9_finish --type finish --name debian9_finish
 
-hammer template list | grep coreos_finish ||
+hammer template list | grep -q coreos_finish ||
   hammer template create --file Template_Scripts/coreos_finish --type finish --name coreos_finish
 
-su -c "ls /usr/share/foreman/.ssh/id_rsa.pub || ssh-keygen -t rsa -f /usr/share/foreman/.ssh/id_rsa -q -P \"\"" -s /bin/bash foreman
+test -f /usr/share/foreman/.ssh/id_rsa.pub || ssh-keygen -t rsa -f /usr/share/foreman/.ssh/id_rsa -q -P \"\"
 cat > /usr/share/foreman/.ssh/config << EOF
 Host *
    StrictHostKeyChecking no
@@ -44,7 +41,5 @@ chown -R foreman. /usr/share/foreman/.ssh
 chmod 0700 /usr/share/foreman/.ssh
 chmod 0600 /usr/share/foreman/.ssh/config
 
-su -c "ssh-copy-id $libvirt_user@$libvirt_host" -s /bin/bash foreman
-
-hammer compute-resource list | grep $libvirt_host ||
+hammer compute-resource list | grep -q $libvirt_host ||
   hammer compute-resource create --provider Libvirt --name $libvirt_host --url qemu+ssh://$libvirt_user@$libvirt_host/system
