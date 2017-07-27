bda68a57f1247222cb13b0899112621911c6f96b
NarigaCDO <narigacdo@gmail.com>
2017-07-27 13:39:59 -0300

    rhel template com variavel

--- foreman/Hammer_Scripts/add_rhel.sh
+++ foreman/Hammer_Scripts/add_rhel.sh
@@ -7,3 +7,6 @@ do
         hammer template add-operatingsystem --id $i --operatingsystem-id $(hammer os list | grep "RHEL Server 7.3" | awk '{print $1}')
         hammer os set-default-template --config-template-id $i --id $(hammer os list | grep "RHEL Server 7.3" | awk '{print $1}')
 done
+
+hammer template list | grep -q rhel_finish ||
+  hammer template create --file Template_Scripts/rhel_finish.erb --type finish --name rhel_finish
