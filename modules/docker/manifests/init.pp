ad62dbf78161a9cdd781adbaf3764856008376bf
usuario de gerencia <nariga@client-desk.home.jab>
2018-01-21 23:53:45 -0200

    dsadasd

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -14,8 +14,8 @@ class docker {
 		path    => '/bin:/usr/bin',
 		unless  => "apt-key list | grep '${key}' | grep -v expired",
 		command => 'add-apt-repository \
-   		    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   		    $(lsb_release -cs) stable"',
+   		    \"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   		    $(lsb_release -cs) stable\"',
 	}
 	->
 	$key = '0EBFCD88'
