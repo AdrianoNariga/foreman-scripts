31aee655b4026dc1864c9827eedf29b16ca4fd3b
usuario de gerencia <nariga@client-desk.home.jab>
2018-01-22 00:29:25 -0200

    dsadasd

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -5,16 +5,17 @@ class docker {
 	elsif $operatingsystem == 'Ubuntu' {
 		$dependencies = [ 'apt-transport-https', 'ca-certificates', 'curl', 'software-properties-common' ]
 		$packages = 'docker-ce'
+		$key = '0EBFCD88'
 	}
 	elsif $operatingsystem == 'CentOS' {
 		$packages = 'docker-engine'
 	}
 
 	file { 'docker.list':
-  		ensure => '/etc/apt/sources.list.d/docker.list',
+  		path => '/etc/apt/sources.list.d/docker.list',
 		content => "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 	}
-	$key = '0EBFCD88'
+	->
 	exec { 'apt-key docker':
 		path    => '/bin:/usr/bin',
 		unless  => "apt-key list | grep '${key}' | grep -v expired",
