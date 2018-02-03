2293a785e01c7ad435e477143cd0f3a9837fa269
usuario de gerencia <nariga@ideapad.home.jab>
2018-02-03 01:41:10 -0200

    zabbix shell deploy

---
+++ libvirtd/clear_instances_lab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+foreman_name=manager
+domain="home.lab"
+smart_proxys="dhcp dns tftp puppet"
+
+virsh list --all | grep running | awk '{print $2}' | while read i
+do
+	virsh destroy $i
+	virsh undefine $i --remove-all-storage
+done
