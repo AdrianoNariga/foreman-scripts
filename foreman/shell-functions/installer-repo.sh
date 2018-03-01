f058dedd03f5c88bd17864211ae37c5a03f69344
usuario de gerencia <nariga@ideapad.home.jab>
2018-03-01 15:29:33 -0300

    mudando repositorio de pacotes

--- foreman/shell-functions/installer-repo.sh
+++ foreman/shell-functions/installer-repo.sh
@@ -5,15 +5,15 @@ case `get_so -s` in
 	CentOS)
 		yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 		yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-		yum -y install https://yum.theforeman.org/releases/1.15/el7/x86_64/foreman-release.rpm
+		yum -y install https://yum.theforeman.org/releases/1.16/el7/x86_64/foreman-release.rpm
 		yum install -y dos2unix foreman-installer
 	;;
 	Ubuntu)
 		apt-get -y install ca-certificates
 		wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 		dpkg -i puppetlabs-release-pc1-xenial.deb
-		echo "deb http://deb.theforeman.org/ xenial 1.15" > /etc/apt/sources.list.d/foreman.list
-		echo "deb http://deb.theforeman.org/ plugins 1.15" >> /etc/apt/sources.list.d/foreman.list
+		echo "deb http://deb.theforeman.org/ xenial 1.16" > /etc/apt/sources.list.d/foreman.list
+		echo "deb http://deb.theforeman.org/ plugins 1.16" >> /etc/apt/sources.list.d/foreman.list
 		apt-get -y install ca-certificates
 		wget -q https://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 		apt-get update
@@ -24,8 +24,8 @@ case `get_so -s` in
 		apt-get -y install ca-certificates
 		wget https://apt.puppetlabs.com/puppetlabs-release-pc1-jessie.deb
 		dpkg -i puppetlabs-release-pc1-jessie.deb
-		echo "deb http://deb.theforeman.org/ jessie 1.15" > /etc/apt/sources.list.d/foreman.list
-		echo "deb http://deb.theforeman.org/ plugins 1.15" >> /etc/apt/sources.list.d/foreman.list
+		echo "deb http://deb.theforeman.org/ jessie 1.16" > /etc/apt/sources.list.d/foreman.list
+		echo "deb http://deb.theforeman.org/ plugins 1.16" >> /etc/apt/sources.list.d/foreman.list
 		apt-get -y install ca-certificates
 		wget -q https://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 		apt-get update
