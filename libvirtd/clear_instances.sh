729b7ae8291e60d862573a0e2667188956ce4e36
usuario de gerencia <nariga@ideapad.home.jab>
2018-03-23 00:35:19 -0300

    remove snapshot

--- libvirtd/clear_instances.sh
+++ libvirtd/clear_instances.sh
@@ -5,6 +5,7 @@ smart_proxys="dhcp dns tftp puppet"
 
 virsh list --all | egrep 'running|shut' | awk '{print $2}' | while read i
 do
+	virsh snapshot-delete $i --current
 	virsh destroy $i
 	virsh undefine $i --remove-all-storage
 done
