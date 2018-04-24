89b3c178f64155844eabac5b239c3a7f22c97a8e
root <root@katello.lab.intranet>
2018-04-24 20:10:01 -0300

    katello deploy

--- katello/server-katello.sh
+++ katello/server-katello.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-yum -y localinstall http://fedorapeople.org/groups/katello/releases/yum/3.4/katello/el7/x86_64/katello-repos-latest.rpm
-yum -y localinstall http://yum.theforeman.org/releases/1.15/el7/x86_64/foreman-release.rpm
+source $1
+yum -y localinstall http://fedorapeople.org/groups/katello/releases/yum/3.6/katello/el7/x86_64/katello-repos-latest.rpm
+yum -y localinstall http://yum.theforeman.org/releases/1.17/el7/x86_64/foreman-release.rpm
 yum -y localinstall https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-
 yum -y localinstall http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y install foreman-release-scl
-yum -y update
-yum -y install katello
+yum -y install foreman-release-scl python-djangoyum
+yum -y install katello yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce
+
+foreman-installer --scenario katello \
+	--enable-foreman-compute-libvirt \
+	--enable-foreman-compute-openstack \
+	--enable-foreman-compute-vmware \
+	--enable-foreman-plugin-ansible \
+	--enable-foreman-proxy-plugin-ansible \
+	--foreman-proxy-dhcp=true \
+	--foreman-proxy-dhcp-interface=$dhcp_iface \
+	--foreman-proxy-dhcp-gateway=$gateway \
+	--foreman-proxy-dhcp-range="$range" \
+	--foreman-proxy-dhcp-nameservers="$dns" \
+	--foreman-proxy-dhcp-pxeserver="$ip_tftp" \
+	--foreman-proxy-tftp=true \
+	--foreman-proxy-tftp-servername=$ip_tftp \
+	--foreman-proxy-dns=true \
+	--foreman-proxy-dns-interface=$dns_iface \
+	--foreman-proxy-dns-zone=$zona_name \
+	--foreman-proxy-dns-reverse=$reverse.in-addr.arpa \
+	--foreman-proxy-dns-forwarders=$dns_forward \
+	--foreman-proxy-dns-forwarders=8.8.4.4
