cf9a737b3c145d21b537ab93eace07d0a5697a89
usuario master <root@katello.home.stc>
2018-05-18 20:05:26 -0300

    asdadasd

--- katello/server-posinstall/04-template.sh
+++ katello/server-posinstall/04-template.sh
@@ -12,3 +12,45 @@ do
                 --name $(echo $i | cut -d '.' -f 1)
 done
 cd -
+
+cd ../Templates_Scripts/finish/
+for i in *
+do
+	echo $i
+	echo
+        hammer template create \
+                --file $i \
+                --type finish \
+                --organizations home \
+                --locations stc \
+                --name $(echo $i | cut -d '.' -f 1)
+done
+cd -
+
+echo "configurando cento 6.9"
+for i in $(hammer template list | grep Kickstart | egrep -v 'finish|Atomic' | awk '{print $1}')
+do
+        hammer template add-operatingsystem --id $i --operatingsystem-id $(hammer os list | grep "CentOS 6.9" | awk '{print $1}')
+        hammer os set-default-template --config-template-id $i --id $(hammer os list | grep "CentOS 6.9" | awk '{print $1}')
+done
+
+echo "configuranfo red hat"
+for i in $(hammer template list | grep Kickstart | egrep -v 'finish|Atomic' | awk '{print $1}')
+do
+        hammer template add-operatingsystem --id $i --operatingsystem-id $(hammer os list | grep "RHEL Server 7.4" | awk '{print $1}')
+        hammer os set-default-template --config-template-id $i --id $(hammer os list | grep "RHEL Server 7.4" | awk '{print $1}')
+done
+
+echo "configurando ubuntu xenial"
+for i in $(hammer template list | grep Kickstart | egrep -v 'finish|Atomic' | awk '{print $1}')
+do
+        hammer template add-operatingsystem --id $i --operatingsystem-id $(hammer os list | grep "Ubuntu Xenial" | awk '{print $1}')
+        hammer os set-default-template --config-template-id $i --id $(hammer os list | grep "Ubuntu Xenial" | awk '{print $1}')
+done
+
+echo "configurando debian stretch"
+for i in $(hammer template list | grep Kickstart | egrep -v 'finish|Atomic' | awk '{print $1}')
+do
+        hammer template add-operatingsystem --id $i --operatingsystem-id $(hammer os list | grep "Debian Stretch" | awk '{print $1}')
+        hammer os set-default-template --config-template-id $i --id $(hammer os list | grep "Debian Stretch" | awk '{print $1}')
+done
