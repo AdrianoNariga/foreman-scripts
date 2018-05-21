d9a606a6229fda4ca517b9d1cf57d5399c109f05
usuario master <root@katello.home.stc>
2018-05-21 12:28:30 -0300

    path comando ip centos6

--- modules/utilsconfs/manifests/gitlabaccess.pp
+++ modules/utilsconfs/manifests/gitlabaccess.pp
@@ -1,6 +1,6 @@
 class utilsconfs::gitlabaccess {
         exec { 'route-to-vpn':
-                path    => '/bin:/usr/bin:/usr/sbin',
+                path    => '/bin:/usr/bin:/usr/sbin:/sbin',
                 unless  => "ip r s | grep 192.168.21.0",
                 command => "ip route add 192.168.21.0/28 via 192.168.111.252",
         }
